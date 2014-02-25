61f8ad97703257e9e5bc1489ba9321828afc26f0
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-02-25 22:33:29 +0800

    support multiple controllers

---
+++ controller/example.php
@@ -0,0 +1,43 @@
+<?php
+
+class Example extends PigController{
+    function index(){
+        $this->render();
+    }
+    
+    function service(){
+        $this->render();
+    }
+    
+    function product(){
+        $array = array(
+        "name" => "Harry Potter and the Prisoner of Azkaban",
+        "author" => "J. K. Rowling",
+        "publisher" => "Arthur A. Levine Books",
+        "amazon_link" => "http://www.amazon.com/dp/0439136369/"
+        );
+         
+        $book = (object) $array;
+
+        $array2 = array(
+        "name" => "SEX CITY",
+        "author" => "J. K. Rowling",
+        "publisher" => "Arthur A. Levine Books",
+        "amazon_link" => "http://www.amazon.com/dp/0439136369/"
+        );
+         
+        $book2 = (object) $array2;
+        
+        $books = array($book, $book2);
+        
+        $data = array('books'=>$books);
+
+        $this->render('', $data);
+    }
+    
+    function contact(){
+        $this->render();
+    }
+
+  
+}
--- index.php
+++ index.php
@@ -14,12 +14,7 @@
  * }
  * ------------------------------------------------------------------
  * 
-class PigController{
-    static $current_action = null;
-    function __construct(){
-        $current_action = 'the_action';
-    }
-}
+
 
 class PigFramework(){
     static $current_controller = null;
@@ -31,20 +26,9 @@ class PigFramework(){
 */
  
 class PigFramework{
-    private $current_action = null;
-    
-    function run(){
-        $action = basename($_SERVER['REQUEST_URI']);
+    private $current_controller = null;
+    private $current_method = null;
 
-        if ( $action === ''){
-            $action = 'index';
-        }
-        
-        $this->current_action = $action;
-
-       call_user_func( array( $this, $action ) );      
-    }
-    
     function __construct(){
         // change this to false if the app is in production
         define('IS_DEVELOPING', true);    
@@ -58,7 +42,31 @@ class PigFramework{
         }
        
         define('BASEPATH', __DIR__);
+    }    
+    
+    function run(){
+        # $action = basename($_SERVER['REQUEST_URI']);
+        $url_segments = explode('/', $_SERVER['REQUEST_URI']);
+        $controller_name = $url_segments[2];
+        $method_name = $url_segments[3];
+
+        if ( $method_name === ''){
+            $method_name = 'index';
+        }
+        
+        $this->current_controller = $controller_name;
+        $this->current_method = $method_name;
+
+        require BASEPATH . '/controller/pig_controller.php';
+        require BASEPATH . '/controller/' . $controller_name . '.php';
+        
+        $class_name = ucfirst($controller_name);
+        $controller = new $class_name($method_name);
+  
+        call_user_func( array( $controller, $method_name ) );      
     }
+    
+
     
     function enable_production_mode(){
         // actually, I don't know how to set them ..
@@ -70,77 +78,6 @@ class PigFramework{
     }
     
 
-    function index(){
-        $this->render();
-    }
-    
-    function service(){
-        $this->render();
-    }
-    
-    function product(){
-        $array = array(
-        "name" => "Harry Potter and the Prisoner of Azkaban",
-        "author" => "J. K. Rowling",
-        "publisher" => "Arthur A. Levine Books",
-        "amazon_link" => "http://www.amazon.com/dp/0439136369/"
-        );
-         
-        $book = (object) $array;
-
-        $array2 = array(
-        "name" => "SEX CITY",
-        "author" => "J. K. Rowling",
-        "publisher" => "Arthur A. Levine Books",
-        "amazon_link" => "http://www.amazon.com/dp/0439136369/"
-        );
-         
-        $book2 = (object) $array2;
-        
-        $books = array($book, $book2);
-        
-        $data = array('books'=>$books);
-
-        $this->render('', $data);
-    }
-    
-    function contact(){
-        $this->render();
-    }
-    
-    function render($file_path = '', $data = []){
-        $file_name = $this->current_action  . '.php';
-        $file_path = BASEPATH . '/view/' . $file_name;
-        echo $this->fetch($file_path, $data);      
-    }
-
-
-    function fetch($file_path = null, $data = [], $layout = 'normal') {
-        ob_start();
-        extract($data);
-        require($file_path);
-        $main = ob_get_contents();
-        ob_end_clean();                // End buffering and discard
-        
-        ob_start();
-        $layout_file_name = BASEPATH . '/view/layout/' . $layout . '.php';
-        require($layout_file_name);                // Include the file
-        $contents = ob_get_contents(); // Get the contents of the buffer
-        ob_end_clean();                // End buffering and discard
-        return $contents;              // Return the contents
-        
-        /*
-        ob_start();                    // Start output buffering
-        $content = file_get_contents($file_path);
-        
-        
-        $layout_file_name = BASEPATH . '/view/layout/' . $layout . '.php';
-        require($layout_file_name);                // Include the file
-        $contents = ob_get_contents(); // Get the contents of the buffer
-        ob_end_clean();                // End buffering and discard
-        return $contents;              // Return the contents
-        */
-    }  
   
 }
 
