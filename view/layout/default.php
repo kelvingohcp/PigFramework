531fe99b6ca34db174c3ceb3c395fee5c1dc1fa6
Kelvin <kelvingohcp@users.noreply.github.com>
2016-01-19 15:11:24 +0800

    Update default.php
    FIX for the correct URL link after click the buttom

--- view/layout/default.php
+++ view/layout/default.php
@@ -1,8 +1,8 @@
 <meta charset='utf8'>
-<script src="/assets/js/jquery-1.10.2.js"></script>
-<script src="/assets/js/bootstrap.js"></script>
-<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.css">
+<script src="/PigFramework/assets/js/jquery-1.10.2.js"></script>
+<script src="/PigFramework/assets/js/bootstrap.js"></script>
+<link rel="stylesheet" type="text/css" href="/PigFramework/assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/PigFramework/assets/css/font-awesome.css">
 
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
@@ -14,15 +14,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Brand</a>
+      <a class="navbar-brand" href="/PigFramework/index.php">Brand</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-          <li class='active'><a href='/index.php/example/intro'>intro</a></li>
-          <li><a href='/index.php/example/product'>PRODUCT</a></li>
-          <li><a href='/index.php/example/contact'>CONTACT</a></li>
+          <li class='active'><a href='/PigFramework/index.php/example/intro'>intro</a></li>
+          <li><a href='/PigFramework/index.php/example/product'>PRODUCT</a></li>
+          <li><a href='/PigFramework/index.php/example/contact'>CONTACT</a></li>
       </ul>
 
     </div><!-- /.navbar-collapse -->
